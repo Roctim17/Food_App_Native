0e627b2980969e2db06b863dd3d4329abc728902
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-06-03 17:59:55 +0600

    catogeries

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,11 +1,21 @@
 import { View, SafeAreaView } from 'react-native'
 import React from 'react'
 import Header from '../Component/Header'
+import SearchBar from '../Component/SearchBar'
+import Categories from '../Component/Categories'
 
 export default function Home() {
     return (
-        <SafeAreaView>
-            <Header></Header>
+        <SafeAreaView style={{ backgroundColor: "#eee" }}>
+            <View style={{
+                backgroundColor: "#fff",
+                padding: 10,
+
+            }}>
+                <Header></Header>
+                <SearchBar></SearchBar>
+                <Categories></Categories>
+            </View>
         </SafeAreaView>
 
     )

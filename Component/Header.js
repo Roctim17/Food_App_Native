16f763459c52902725fd49e414dff94ac1359f89
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-06-03 15:25:21 +0600

    end header

--- Component/Header.js
+++ Component/Header.js
@@ -1,18 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
+import { useState } from 'react'
 
 export default function Header() {
+    const [active, setActive] = useState("Delivery")
     return (
         <View style={{ flexDirection: "row", alignSelf: "center" }}>
-            <HeaderBtn text="Delivery" />
-            <HeaderBtn text="Pickup" />
+            <HeaderBtn text="Delivery" btnColor="black" textColor="white" active={active} setActive={setActive} />
+            <HeaderBtn text="Pickup" btnColor="white" textColor="black" active={active} setActive={setActive} />
         </View>
     )
 }
 const HeaderBtn = (props) => (
-    <TouchableOpacity>
-        <Text>{props.text}</Text>
+    <TouchableOpacity style={{
+        backgroundColor: props.active == props.text ? "black" : "white",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+        marginVertical: 50,
+
+    }}
+        onPress={() => props.setActive(props.text)}
+    >
+        <Text style={{
+            color: props.active == props.text ? "white" : "black",
+            fontSize: 15,
+            fontWeight: "900"
+        }}>
+            {props.text}
+        </Text>
     </TouchableOpacity>
 )
 
